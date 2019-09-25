23fc7360b488f52d1524d2e28d32002ebc5ebbf4
Daniel Kramer <43589646+DanielAKramer@users.noreply.github.com>
2019-09-25 15:37:04 +0000

    Day 3 lunch

---
+++ src/app/features/gift-giving/actions/friend-holiday-actions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from '@ngrx/store';
+
+
+export const assignHolidayToFriend = createAction(
+  '[gift giving] assign holiday to friend',
+  props<{ friendId: string; holidayId: string }>()
+);
+
+
---
+++ src/app/features/gift-giving/containers/friend-gifts/friend-gifts.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs/operators';
+import { Subscription, Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { GiftGivingState, selectFriendHolidayModel } from '../../reducers';
+import { setSelectFriend } from '../../actions/friend.actions';
+import { FriendHoliday } from '../../models';
+import { assignHolidayToFriend } from '../../actions/friend-holiday-actions';
+
+@Component({
+  selector: 'app-friend-gifts',
+  templateUrl: './friend-gifts.component.html',
+  styleUrls: ['./friend-gifts.component.css']
+})
+export class FriendGiftsComponent implements OnInit, OnDestroy {
+  model$: Observable<FriendHoliday>;
+  subscription: Subscription;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private store: Store<GiftGivingState>) { }
+
+  ngOnInit() {
+    this.model$ = this.store.select(selectFriendHolidayModel);
+    this.subscription = this.activatedRoute.paramMap.pipe(
+      map(params => {
+        this.store.dispatch(setSelectFriend({ id: params.get('id') }));
+      }),
+      tap(() => console.log('Is this thing on?'))
+    ).subscribe();
+  }
+
+  backToList() {
+    this.router.navigate(['/gifts', 'friends']);
+  }
+
+  addHoliday(friendId: string, holidayId: string) {
+    this.store.dispatch(assignHolidayToFriend({ friendId, holidayId }));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+}
