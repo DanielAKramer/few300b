c949f5fc3df249fffd7ce90a50b811f8db5bcf20
Daniel Kramer <43589646+DanielAKramer@users.noreply.github.com>
2019-09-25 18:22:52 +0000

    done?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { reducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,10 +1,12 @@
+import { RouterState, routerReducer } from '@ngrx/router-store';
 
 
 export interface AppState {
+  router: RouterState.Minimal;
 
 }
 
 
 export const reducers = {
-
+  router: routerReducer
 };
